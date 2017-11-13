cb2340a02280b8758429fdbc6f15bc915fcd365d
Sergio Lindo Mansilla <slindomansilla@suse.com>
2017-11-13 15:12:16 +0100

    Add config file for rsyncd

--- webui/run_openqa.sh
+++ webui/run_openqa.sh
@@ -10,6 +10,7 @@ fi
 
 # run services
 dbus-daemon --system --fork
+start_daemon /usr/sbin/rsyncd --daemon --no-detach &
 start_daemon -u geekotest /usr/share/openqa/script/openqa-scheduler &
 start_daemon -u geekotest /usr/share/openqa/script/openqa-websockets &
 start_daemon -u geekotest /usr/share/openqa/script/openqa gru -m production run &
