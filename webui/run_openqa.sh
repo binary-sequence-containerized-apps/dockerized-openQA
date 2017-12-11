dab4b36a5d9664ce8a4f55da8b9aea2806e683b5
Sergio Lindo Mansilla <slindomansilla@suse.com>
2017-12-11 11:30:21 +0100

    webui now uses postgresql

--- webui/run_openqa.sh
+++ webui/run_openqa.sh
@@ -4,16 +4,12 @@ export DBUS_STARTER_BUS_TYPE=system
 
 # prepare environment
 rm -f /run/dbus/pid
-if [ ! -f /data/db/db.sqlite ]; then
-  /usr/share/openqa/script/initdb --user geekotest --init_database
-else
-  /usr/share/openqa/script/upgradedb --user geekotest --upgrade_database
-fi
 
 # run services
 dbus-daemon --system --fork
+start_daemon -u postgres  /usr/share/postgresql/postgresql-script start &
+start_daemon -u root      /usr/sbin/rsyncd --daemon --no-detach &
 start_daemon -u geekotest /usr/share/openqa/script/openqa-resource-allocator &
-start_daemon /usr/sbin/rsyncd --daemon --no-detach &
 start_daemon -u geekotest /usr/share/openqa/script/openqa-scheduler &
 start_daemon -u geekotest /usr/share/openqa/script/openqa-websockets &
 start_daemon -u geekotest /usr/share/openqa/script/openqa gru -m production run &
