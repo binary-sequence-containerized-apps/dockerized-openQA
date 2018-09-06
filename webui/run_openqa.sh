37ec1068063cd95bdeb4db1798dc8d25fb1b49e5
Sergio Lindo Mansilla <slindomansilla@suse.com>
2018-09-06 13:25:20 +0200

    Stop using rsync inside the same container
    - Trying to have microservices

--- webui/run_openqa.sh
+++ webui/run_openqa.sh
@@ -8,7 +8,6 @@ rm -f /run/dbus/pid
 # run services
 dbus-daemon --system --fork
 start_daemon -u postgres  /usr/share/postgresql/postgresql-script start &
-start_daemon -u root      /usr/sbin/rsyncd --daemon --no-detach &
 start_daemon -u geekotest /usr/share/openqa/script/openqa-resource-allocator &
 start_daemon -u geekotest /usr/share/openqa/script/openqa-scheduler &
 start_daemon -u geekotest /usr/share/openqa/script/openqa-websockets &
